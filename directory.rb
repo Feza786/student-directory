e318bade578ff16f9310c9c6caafbb63b3d42ec9
Feza Bukhari <fiza.bukhari15@gmail.com>
2017-01-16 11:12:39 +0000

    We have 11 Students

--- directory.rb
+++ directory.rb
@@ -12,3 +12,11 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+#puts adds a line return after printing each argument (the cursor wraps onto the next line)
+print "Overall, we have "
+# the print method does not wrap automatically, it doesnt add new line characters.
+print 11
+puts " great students"
+
+#you can put the line break character manually by using an escape sequence:
+#eg print "line1\nline2\nline3"
