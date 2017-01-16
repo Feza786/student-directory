678332cb374ed156042892a2d452daa96348bb23
Feza Bukhari <fiza.bukhari15@gmail.com>
2017-01-16 11:34:02 +0000

    changes

--- directory.rb
+++ directory.rb
@@ -1,4 +1,5 @@
-
+student_count = 11
+#first we print the list of students
 puts "The students of Villains Academy"
 puts "............"
 puts "Dr. Hannibal Lecter"
@@ -12,11 +13,5 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
-#puts adds a line return after printing each argument (the cursor wraps onto the next line)
-print "Overall, we have "
-# the print method does not wrap automatically, it doesnt add new line characters.
-print 11
-puts " great students"
-
-#you can put the line break character manually by using an escape sequence:
-#eg print "line1\nline2\nline3"
+#finally we print the total number of students
+puts "Overall, we have #{student_count} great students"
